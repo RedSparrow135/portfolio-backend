b48f3fefe092fa0f7955bbbe1a585661b9d85a2c
RedSparrow135 <alonsopicho@gmail.com>
2025-12-24 17:27:46 -0300

    DE NUEVI

--- src/server.js
+++ src/server.js
@@ -1,39 +1,20 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import contactRoutes from "./routes/contact.js";
+
+dotenv.config();
 
 const app = express();
 
-/* FRONTENDS PERMITIDOS */
-const allowedOrigins = [
-  "http://localhost:5173",
-  // cuando tengas frontend en prod, lo agregas aquí
- "https://portfolio-backend-production-99fb.up.railway.app/"
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Permitir Postman / curl
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, origin);
-      }
-
-      return callback(new Error("CORS bloqueado"));
-    },
-    methods: ["POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
-
+app.use(cors({
+  origin: "http://localhost:5173", // tu React
+}));
 app.use(express.json());
+
 app.use("/api/contact", contactRoutes);
 
-/* 🚨 ESTO ES CLAVE EN RAILWAY */
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("🚀 Backend escuchando en puerto", PORT);
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`🚀 Backend en http://localhost:${PORT}`);
 });
